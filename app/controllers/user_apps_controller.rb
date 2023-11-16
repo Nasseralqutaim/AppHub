f3ddb8fcd1ea5ff22649ad7dedaccc83ab408096
Nasser Alqutaim <n.alqutaim@gmail.com>
2023-11-15 22:19:19 -0700

    add flash method to fix the add app option bug

--- app/controllers/user_apps_controller.rb
+++ app/controllers/user_apps_controller.rb
@@ -1,18 +1,25 @@
 class UserAppsController < ApplicationController
-  # POST /user_apps
-  
-  def create
-    user = User.first 
-    app_ids = params[:app_ids] 
-    app_ids.each do |app_id|
-      UserApp.create(user_id: user.id, app_id: app_id)
-      flash[:notice] = 'Apps added successfully.'
-      redirect_to '/home'
+ # POST /user_apps
+ def create
+  user = User.first 
+  app_ids = params[:app_ids] 
+  app_ids.each do |app_id|
+    UserApp.create(user_id: user.id, app_id: app_id)
   end
+
+  # Set the flash message
+  flash[:notice] = 'Apps added successfully.'
+
+  # Redirect to the home page
+  redirect_to '/home'
+end
+
+
  # DELETE /user_apps/:id
  def destroy
   @user_app = UserApp.find(params[:id])
   @user_app.destroy
   redirect_to '/home', notice: 'App removed successfully.'
 end 
+
 end
