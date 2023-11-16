7e9c6b14b1c16bfc7333b9e3de1c56d1f23d79e3
Nasser Alqutaim <n.alqutaim@gmail.com>
2023-11-15 18:07:04 -0700

    add create action to UserApps controller

--- app/controllers/user_apps_controller.rb
+++ app/controllers/user_apps_controller.rb
@@ -1,2 +1,13 @@
 class UserAppsController < ApplicationController
+  # POST /user_apps
+  
+  def create
+    user = User.first 
+    app_ids = params[:app_ids] 
+    app_ids.each do |app_id|
+      UserApp.create(user_id: user.id, app_id: app_id)
+    end
+    redirect_to '/home', flash: { notice: 'Apps added successfully.' }
+  end
+  
 end
