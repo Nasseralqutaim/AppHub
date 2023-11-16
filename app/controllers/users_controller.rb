549d3cf370835686e9b33bb38b65d767aa97e9e3
Nasser Alqutaim <n.alqutaim@gmail.com>
2023-11-15 18:02:41 -0700

    edit home action to fetch available apps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def home
     user = User.first 
     @user_apps = user.user_apps.includes(:app) if user
+    @available_apps = App.where.not(id: user.apps.select(:id))
   end
 end
