c415dec1491bc818d6ea899e2a26bb6cee14b490
Nasser Alqutaim <n.alqutaim@gmail.com>
2023-12-08 09:49:26 -0700

    Fix include preloaded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,31 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+users = [
+  { login: 'user1', password: 'glbrcpass' },
+  { login: 'user2', password: 'glbrcpass' },
+  { login: 'user3', password: 'glbrcpass' }
+]
+
+users.each do |user|
+  User.find_or_create_by(login: user[:login]) do |u|
+    u.password = user[:password]
+  end
+end
+
+# Apps
+apps = [
+  { name: "Google", description: "Search Engine", color: "Red", default_status: true, link: "http://www.google.com", shape: "shape-circle" },
+  { name: "Wisc", description: "UW", color: "Blue", default_status: false, link: "http://www.wisc.edu", shape: "shape-diamond" },
+  { name: "GLBRC", description: "Great Lakes Bioenergy Research Center", color: "Yellow", default_status: true, link: "http://www.glbrc.org", shape: "shape-triangle" },
+  { name: "WEI", description: "Wisconsin Energy", color: "Green", default_status: false, link: "https://energy.wisc.edu/", shape: "shape-square" },
+  { name: "Twitter", description: "Twitter", color: "Purple", default_status: false, link: "https://twitter.com/", shape: "shape-diamond" }
+]
+
+apps.each do |app|
+  App.find_or_create_by(name: app[:name]) do |a|
+    a.description = app[:description]
+    a.color = app[:color]
+    a.default_status = app[:default_status]
+    a.link = app[:link]
+    a.shape = app[:shape]
+  end
+end
